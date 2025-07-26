11175ca8fd9bfd4697a50b17b3f0b12bcb4bdda9
RaviDwivedi21 <ravi.dwivedi@wipro.com>
2025-07-26 17:03:06 +0530

    Initial Commit

---
+++ CleanNetworkCallDemo/CleanNetworkCallDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanNetworkCallDemoApp.swift
+//  CleanNetworkCallDemo
+//
+//  Created by Ravi Dwivedi on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanNetworkCallDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanNetworkCallDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanNetworkCallDemo
+//
+//  Created by Ravi Dwivedi on 26/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
